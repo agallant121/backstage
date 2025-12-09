5dd79cdc6897d7fe0efd9c17760ee51e16bb506f
Alex Gallant <agallant1201@gmail.com>
2025-12-09 15:09:55 -0500

    Update index posts query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = current_user.posts.order(created_at: :desc)
+  @posts = Post
+             .joins(:group)
+             .where(group_id: current_user.group_ids)
+             .or(Post.where(user_id: current_user.id))
+             .order(created_at: :desc)  
   end
 end
