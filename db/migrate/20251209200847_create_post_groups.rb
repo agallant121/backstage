976bd087d58b3d2e7fe070f125bfd7a1cdbe6358
Alex Gallant <agallant1201@gmail.com>
2025-12-10 10:35:04 -0500

    Remove null contraint

--- db/migrate/20251209200847_create_post_groups.rb
+++ db/migrate/20251209200847_create_post_groups.rb
@@ -6,7 +6,7 @@ class CreatePostGroups < ActiveRecord::Migration[8.0]
 
       t.timestamps
     end
-    
-    add_index :post_groups, [:post_id, :group_id], unique: true
+
+    add_index :post_groups, [ :post_id, :group_id ], unique: true
   end
 end
