6562403215247000190bf06495e121fcdc2a0757
Alex Gallant <agallant1201@gmail.com>
2025-12-10 10:32:26 -0500

    Simplify home controller groups assignment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  @posts = Post
-             .joins(:group)
-             .where(group_id: current_user.group_ids)
-             .or(Post.where(user_id: current_user.id))
-             .order(created_at: :desc)  
+    @groups = current_user.groups
   end
 end
