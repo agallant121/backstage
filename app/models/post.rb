4843026c88101b97d9e4853371e7bff3832c51b9
Alex Gallant <agallant1201@gmail.com>
2025-12-10 10:33:36 -0500

    Add optional true for group to send to all groups and make body for post required

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
-  belongs_to :group  # keep this for now so old code still works
+  belongs_to :group, optional: true
 
   has_many :post_groups, dependent: :destroy
   has_many :groups, through: :post_groups
 
   has_many_attached :images
+
+  validates :body, presence: true
 end
