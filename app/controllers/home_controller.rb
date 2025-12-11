1fc6cefc861ab20604c94343c85e307a5f7de494
Alex Gallant <agallant1201@gmail.com>
2025-12-11 09:40:49 -0500

    Update controllers for group creation and conditional full or summary views for posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @groups = current_user.groups
+    @groups = current_user.groups.order(created_at: :desc)
   end
 end
