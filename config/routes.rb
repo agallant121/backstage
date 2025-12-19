00f4b7688c81bf93fd108b0f4197a0c9201f7abf
Alex Gallant <agallant1201@gmail.com>
2025-12-19 10:56:31 -0500

    Add users edit back into routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
       request.params[:invite_token].present? || request.session[:invitation_token].present?
     end
 
+    get "users/edit", to: "users/registrations#edit", as: :edit_user_registration
+    patch "users", to: "users/registrations#update"
+    put "users", to: "users/registrations#update"
+
     get "users/sign_up",
         to: "users/registrations#new",
         as: :new_user_registration,
