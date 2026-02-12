082523d5bf1eaf184661b6b21b41b14e9b65b26b
Alex Gallant <agallant1201@gmail.com>
2026-02-12 14:07:09 -0500

    Remove empty space

--- app/controllers/groups/invitations_controller.rb
+++ app/controllers/groups/invitations_controller.rb
@@ -10,8 +10,6 @@ class Groups::InvitationsController < ApplicationController
   def index
     @pending_invitations  = @group.invitations.pending.includes(:inviter).order(created_at: :desc)
     @accepted_invitations = @group.invitations.accepted.includes(:inviter).order(updated_at: :desc)
-
-    
   end
 
   def create
