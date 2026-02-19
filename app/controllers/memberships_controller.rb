d22dbed4a96272ef31dfecca98cc57d573502862
Alex Gallant <agallant1201@gmail.com>
2026-02-19 10:32:17 -0500

    Add redirect for users without group invitations in memberships controller

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -9,6 +9,11 @@ class MembershipsController < ApplicationController
     end
 
     invitation = @group.invitations.pending.find_by(email: current_user.email.downcase)
+    unless invitation
+      redirect_to groups_path, alert: "You have not been invited to join this group."
+      return
+    end
+
     invitation.accept!(current_user)
     redirect_to @group, notice: "Joined group"
   end
