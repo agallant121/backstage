0d7d9b3fe57d3084540e9fc1806edc8e29a05489
Alex Gallant <agallant1201@gmail.com>
2026-03-24 10:57:50 -0400

    Update database schema to add children table and enhance groups table with message summary fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_13_100000) do
+ActiveRecord::Schema[8.0].define(version: 2026_03_24_120000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -42,11 +42,25 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_13_100000) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "children", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.date "birthday"
+    t.integer "age"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_children_on_user_id"
+  end
+
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "message_summary"
+    t.datetime "message_summary_generated_at"
+    t.string "message_summary_source"
   end
 
   create_table "invitations", force: :cascade do |t|
@@ -56,9 +70,9 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_13_100000) do
     t.string "email", null: false
     t.string "token", null: false
     t.datetime "accepted_at"
-    t.datetime "expires_at", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "expires_at", null: false
     t.index ["email"], name: "index_invitations_on_email"
     t.index ["expires_at"], name: "index_invitations_on_expires_at"
     t.index ["group_id"], name: "index_invitations_on_group_id"
@@ -101,13 +115,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_13_100000) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
@@ -117,25 +124,22 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_13_100000) do
     t.date "spouse_birthday"
     t.text "home_address"
     t.text "contact_notes"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
-  create_table "children", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "name", null: false
-    t.date "birthday"
-    t.integer "age"
-    t.text "notes"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_children_on_user_id"
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "children", "users"
   add_foreign_key "invitations", "groups"
   add_foreign_key "invitations", "users", column: "invited_user_id"
   add_foreign_key "invitations", "users", column: "inviter_id"
@@ -144,5 +148,4 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_13_100000) do
   add_foreign_key "post_groups", "groups"
   add_foreign_key "post_groups", "posts"
   add_foreign_key "posts", "users"
-  add_foreign_key "children", "users"
 end
