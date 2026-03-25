f1d53dd9dba9d613e4167f43158d09ad3560db97
Alex Gallant <agallant1201@gmail.com>
2026-03-25 14:28:29 -0400

    Add tests for per-post expand controls in compact mode and refactor post list rendering

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
+  test "index renders visible per-post expand controls in compact mode" do
+    expandable_post = Post.create!(
+      body: "Expanded post body " * 20,
+      user: @user,
+      groups: [ groups(:one) ]
+    )
+
+    short_post = Post.create!(
+      body: "Short body",
+      user: @user,
+      groups: [ groups(:one) ]
+    )
+
+    get posts_path
+
+    assert_response :success
+    assert_select "button[data-action='post-expand#expand']", text: "Read full post", count: 1
+    assert_includes response.body, expandable_post.body.truncate(160)
+    assert_select "#post_#{short_post.id} button[data-action='post-expand#expand']", count: 0
+  end
 end
