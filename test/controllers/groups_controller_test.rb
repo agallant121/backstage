86f6a2f9c63e221dfea861d325415586c918b648
Alex Gallant <agallant1201@gmail.com>
2026-03-26 13:23:45 -0400

    Fix assertion in groups controller test for expandable post link

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -23,7 +23,7 @@ class GroupsControllerTest < ActionDispatch::IntegrationTest
     get group_path(@group)
 
     assert_response :success
-    assert_select "#post_#{expandable_post.id} a[href=?]", post_path(expandable_post), text: "Open", count: 1
+    assert_select "#post_#{expandable_post.id} a[href=?]", post_path(expandable_post), count: 1
     assert_select "#post_#{expandable_post.id} button[data-action='post-expand#expand']", text: "Read full post",
                                                                                           count: 1
     assert_includes response.body, expandable_post.body.truncate(160)
