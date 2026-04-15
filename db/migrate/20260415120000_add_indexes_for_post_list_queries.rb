27b6e352b4b3cb9415d20cc768253f4cb7592200
Alex Gallant <agallant1201@gmail.com>
2026-04-15 16:16:27 -0400

    Refactor index management in post list queries migration for concurrent operations

--- db/migrate/20260415120000_add_indexes_for_post_list_queries.rb
+++ db/migrate/20260415120000_add_indexes_for_post_list_queries.rb
@@ -1,8 +1,15 @@
 class AddIndexesForPostListQueries < ActiveRecord::Migration[8.0]
-  def change
-    remove_index :post_groups, :group_id
+  disable_ddl_transaction!
 
-    add_index :post_groups, [ :group_id, :post_id ]
-    add_index :posts, :created_at
+  def up
+    add_index :post_groups, [ :group_id, :post_id ], algorithm: :concurrently
+    remove_index :post_groups, column: :group_id, algorithm: :concurrently
+    add_index :posts, :created_at, algorithm: :concurrently
+  end
+
+  def down
+    add_index :post_groups, :group_id, algorithm: :concurrently
+    remove_index :post_groups, column: [ :group_id, :post_id ], algorithm: :concurrently
+    remove_index :posts, column: :created_at, algorithm: :concurrently
   end
 end
